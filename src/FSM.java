8f149592e4276c7d165590f430da9a20c665a8e8
Ramazan-seng <ramazanyldrmcm@gmail.com>
2025-04-21 22:07:33 +0300

    FSM class added

--- src/FSM.java
+++ src/FSM.java
@@ -1,2 +1,11 @@
+import java.util.*;
+
 public class FSM {
+    private Set<String> symbols = new HashSet<>();
+    private Map<String, State> states = new HashMap<>();
+    private State initialState;
+    private Set<State> finalStates = new HashSet<>();
+    private Map<String, Map<String, State>> transitions = new HashMap<>();
+
 }
+
